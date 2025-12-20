7926f7bf19990187092d40b8b57a294ba3679598
Mcm190 <maxcartermarshall@gmail.com>
2025-12-20 12:19:19 +0000

    Initial Commit

---
+++ iOSchan/iOSchanApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSchanApp.swift
+//  iOSchan
+//
+//  Created by MCM on 20/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSchanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
